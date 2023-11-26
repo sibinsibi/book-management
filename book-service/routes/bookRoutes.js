bcdf31d0a9569fe8ed0c72200dedf73c55daaee1
Sibin <asibin99@gmail.com>
2023-11-26 12:41:50 +0530

    completed book management

--- book-service/routes/bookRoutes.js
+++ book-service/routes/bookRoutes.js
@@ -1,12 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getAllBooks, getBookById, addBook, updateBook, deleteBook } = require('../controllers/bookController');
-const { verifyToken, authorizeAdmin } = require('../../shared/middlewares/authMiddleware');
+const {
+  getAllBooks,
+  getBookById,
+  addBook,
+  updateBook,
+  deleteBook,
+} = require("../controllers/bookController");
+const {
+  verifyToken,
+  authorizeAdmin,
+} = require("../../shared/middlewares/authMiddleware");
 
-router.get('/', getAllBooks);
-router.get('/:id', getBookById);
-router.post('/addBook', verifyToken, authorizeAdmin, addBook);
-router.put('/:id', updateBook);
-router.delete('/:id', deleteBook);
+router.get("/", verifyToken, getAllBooks);
+router.get("/:id", verifyToken, getBookById);
+router.post("/addBook", verifyToken, authorizeAdmin, addBook);
+router.put("/:id", verifyToken, authorizeAdmin, updateBook);
+router.delete("/:id",  verifyToken, authorizeAdmin, deleteBook);
 
 module.exports = router;
